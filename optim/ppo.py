087b525c3744f7461c0ec30c67a0eca3257db488
Ben Glickenhaus <benglickenhaus@gmail.com>
2020-09-02 10:49:37 -0400

    getting there
    tbd how much faster it is

--- optim/ppo.py
+++ optim/ppo.py
@@ -8,6 +8,7 @@ from typing import Dict, List, Tuple
 from policies.base_neural_net import BaseNeuralNet
 from tqdm import tqdm, trange
 import copy
+import torch.multiprocessing as mp
 
 torch.autograd.set_detect_anomaly(True)
 
@@ -36,6 +37,14 @@ class Memory(Dataset):
         self.advantages += advantages
         self.hcs += hcs
 
+    def add_memory(self, memory):
+        self.states += memory.states
+        self.actions += memory.actions
+        self.old_logprobs += memory.old_logprobs
+        self.rewards += memory.rewards
+        self.advantages += memory.advantages
+        self.hcs += memory.hcs
+
     def clear_memory(self):
         del self.states[:]
         del self.actions[:]
@@ -43,6 +52,67 @@ class Memory(Dataset):
         del self.rewards[:]
         del self.advantages[:]
         del self.hcs[:]
+
+
+def rollout(env_config, key, constructor, state_dict, n_rollouts, num_steps):
+    env = foundation.make_env_instance(**env_config)
+    obs = env.reset()
+    t = trange(n_rollouts, desc='Rollout')
+    memory = Memory()
+    model = constructor(device='cpu')
+    model.build_models(ObservationBatch(obs, key))
+    model.load_state_dict(state_dict, strict=False)
+    for rollout in t:
+        obs = env.reset()
+        states, actions, logprobs, rewards, values, done, hcs = [], [], [], [], [], [], []
+        hc = (torch.zeros(1, len(key), model.lstm_size, device='cpu'),
+              torch.zeros(1, len(key), model.lstm_size, device='cpu'))
+        for step in range(num_steps):
+            obs_batch = ObservationBatch(obs, key)
+            hcs.append(hc)
+            dist, value, hc = model(obs_batch, hc)
+            hc = (hc[0].detach(), hc[1].detach())
+            a = dist.sample().detach()
+            value = value.squeeze()
+            logprob = dist.log_prob(a).detach()
+            action_dict = dict((i, a.detach().cpu().numpy()) for i, a in zip(obs_batch.order, a.argmax(-1)))
+            next_obs, rew, is_done, info = env.step(action_dict)
+            states.append(obs_batch)
+            actions.append(a.argmax(-1).detach())
+            logprobs.append(logprob)
+            rewards.append(np.array([rew[k] for k in key]))
+            values.append(value)
+            done.append(is_done['__all__'])
+
+            obs = next_obs
+
+        obs_batch = ObservationBatch(obs, key)
+        _, next_value, hc = model(obs_batch, hc)
+        next_value = next_value.detach().cpu().numpy()
+        values = torch.stack(values).detach().cpu().numpy()
+        discounted_rewards = compute_gae(next_value, rewards, done, values)
+        advantage = (discounted_rewards - values).tolist()
+        discounted_rewards = discounted_rewards.tolist()
+        memory.add_trace(states, actions, logprobs, discounted_rewards, advantage, hcs)
+        return memory
+
+
+def compute_gae(next_value, rewards, masks, values, tau=0.95, gamma=0.99):
+    values = np.vstack((values, next_value.T))
+    gae = 0
+    returns = []
+    for step in reversed(range(len(rewards))):
+        delta = rewards[step] + (gamma * values[step + 1] * masks[step]).squeeze() - values[step]
+        gae = delta + gamma * tau * masks[step] * gae
+        returns.insert(0, gae + values[step])
+    return np.array(returns)
+
+
+def join_memories(memories: List[Memory]):
+    out = Memory()
+    for memory in memories:
+        out.add_memory(memory)
+    return out
 
 
 class PPO:
@@ -53,6 +123,7 @@ class PPO:
         self.env_config = env_config
         env = foundation.make_env_instance(**env_config)
         self.models = dict()
+        self.model_key_to_constructor = models
         obs = env.reset()
         for key, value in models.items():
             self.models[key] = value(device=self.device)
@@ -67,17 +138,9 @@ class PPO:
         for key, value in self.models.items():
             self.optimizers[key] = torch.optim.Adam(value.parameters(), lr=self.lr)
 
-    def compute_gae(self, next_value, rewards, masks, values, tau=0.95):
-        values = np.vstack((values, next_value.T))
-        gae = 0
-        returns = []
-        for step in reversed(range(len(rewards))):
-            delta = rewards[step] + (self.gamma * values[step + 1] * masks[step]).squeeze() - values[step]
-            gae = delta + self.gamma * tau * masks[step] * gae
-            returns.insert(0, gae + values[step])
-        return np.array(returns)
-
     def update(self, key, epochs, batch_size=1, shuffle=False, num_workers=0):
+        for p in self.models[key].parameters():
+            p.requires_grad = True
         losses = []
         for epoch in range(epochs):
             dataloader = DataLoader(self.memory[key], batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
@@ -114,47 +177,15 @@ class PPO:
                 self.optimizers[key].step()
         return losses
 
-    def rollout(self, key, n_rollouts, num_steps):
-        env = foundation.make_env_instance(**self.env_config)
-        t = trange(n_rollouts, desc='Rollout')
-        for rollout in t:
-            obs = env.reset()
-            states, actions, logprobs, rewards, values, done, hcs = [], [], [], [], [], [], []
-            hc = (torch.zeros(1, len(key), self.models[key].lstm_size, device=self.device),
-                  torch.zeros(1, len(key), self.models[key].lstm_size, device=self.device))
-            for step in range(num_steps):
-                obs_batch = ObservationBatch(obs, key)
-                hcs.append(hc)
-                dist, value, hc = self.models[key](obs_batch, hc)
-                hc = (hc[0].detach(), hc[1].detach())
-                a = dist.sample().detach()
-                value = value.squeeze()
-                logprob = dist.log_prob(a).detach()
-                action_dict = dict((i, a.detach().cpu().numpy()) for i, a in zip(obs_batch.order, a.argmax(-1)))
-                next_obs, rew, is_done, info = env.step(action_dict)
-                states.append(obs_batch)
-                actions.append(a.argmax(-1).detach())
-                logprobs.append(logprob)
-                rewards.append(np.array([rew[k] for k in key]))
-                values.append(value)
-                done.append(is_done['__all__'])
-
-                obs = next_obs
-
-            obs_batch = ObservationBatch(obs, key)
-            _, next_value, hc = self.models[key](obs_batch, hc)
-            next_value = next_value.detach().cpu().numpy()
-            values = torch.stack(values).detach().cpu().numpy()
-            discounted_rewards = self.compute_gae(next_value, rewards, done, values)
-            advantage = (discounted_rewards - values).tolist()
-            discounted_rewards = discounted_rewards.tolist()
-            self.memory[key].add_trace(states, actions, logprobs, discounted_rewards, advantage, hcs)
-
-    def train(self, key_order: List[Tuple[Tuple, Dict]], n_training_steps):
+    def train(self, key_order: List[Tuple[Tuple, Dict]], n_training_steps, n_jobs=1):
         t = trange(n_training_steps, desc='Training Iteration', leave=True)
         for it in t:
             for key, spec in key_order:
-                self.rollout(key, spec.get('n_rollouts'), spec.get('n_steps_per_rollout'))
+                constructor = self.model_key_to_constructor[key]
+                state_dict = self.models[key].to('cpu').state_dict()
+                result = mp.Pool(n_jobs).starmap(rollout, [(self.env_config, key, constructor, state_dict, 1, spec.get('n_steps_per_rollout')) for _ in range(spec.get('n_rollouts'))])
+                result = join_memories(result)
+                # self.rollout(key, spec.get('n_rollouts'), spec.get('n_steps_per_rollout'))
                 losses = self.update(key, spec.get('epochs_per_train_step'), spec.get('batch_size'))
                 self.memory[key].clear_memory()
                 t.set_description(f'Training Iteration (Average Reward: {np.mean(self.memory[key].rewards).round(3)}, Average Loss: {np.mean(losses).round(3)}')
