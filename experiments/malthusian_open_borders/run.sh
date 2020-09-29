99fe0d35db8ece424a2ac0cde421bf5808b209ba
Ben Glickenhaus <benglickenhaus@gmail.com>
2020-09-29 10:09:05 -0400

    queue change

--- experiments/malthusian_open_borders/run.sh
+++ experiments/malthusian_open_borders/run.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=malthusian_open_borders
 #SBATCH --output=res_%j.txt  # output file
 #SBATCH -e res_%j.err        # File to which STDERR will be written
-#SBATCH --partition=2080ti-long # Partition to submit to
+#SBATCH --partition=2080ti-short # Partition to submit to
 #
 #SBATCH --ntasks=24
 #SBATCH --mem-per-cpu=4000    # Memory in MB per cpu allocated
